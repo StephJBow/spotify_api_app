a40ef88cec0721d46ba7e10a1e57371db2dc9583
Stephanie Bowring <stephaniejbowring@gmail.com>
2024-01-28 16:34:36 +0000

    added client credentials with spotify api access

--- spotify_app/src/App.tsx
+++ spotify_app/src/App.tsx
@@ -1,15 +1,57 @@
-
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import { Credentials } from './Credentials';
 
 function App() {
-	const [count, setCount] = useState(0);
+	const spotify = Credentials();
+
+	console.log('RENDERING APP.TSX');
+
+	const [token, setToken] = useState('');
+	const [genres, setGenres] = useState({ selectedGenre: '', listPfGenresFromAPI: [] });
+
+	useEffect(() => {
+		fetch('https://accounts.spotify.com/api/token', {
+			headers: {
+				'Content-Type': 'application/x-www-form-urlencoded',
+				'Authorization': 'Basic ' + btoa(spotify.ClientId + ':' + spotify.ClientSecret)
+			},
+			body: 'grant_type=client_credentials',
+			method: 'POST'
+		})
+			.then(response => {
+				if (!response.ok) {
+					throw new Error('Failed to fetch access token');
+				}
+				return response.json();
+			})
+			.then(tokenResponse => {
+				if (tokenResponse && tokenResponse.access_token) {
+					setToken(tokenResponse.access_token);
+
+					fetch('https://api.spotify.com/v1/browse/categories?locale=sv_US', {
+						method: 'GET',
+						headers: { 'Authorization': 'Bearer ' + tokenResponse.access_token }
+					})
+						.then(genreResponse => {
+							if (!genreResponse.ok) {
+								throw new Error('Failed to fetch genres');
+							}
+							return genreResponse.json();
+						})
+						.then(genreResponseJson => {
+							console.log(genreResponseJson);
+						});
+				}
+			})
+			.catch(error => {
+				console.error('Error:', error);
+			});
+	}, [spotify.ClientId, spotify.ClientSecret]); // Added dependency array
 
 	return (
 		<>
-			<h1>app</h1>
+			<h1>Hello</h1>
 		</>
 	);
 }
---
+++ spotify_app/src/Credentials.tsx
@@ -0,0 +1,9 @@
+const Credentials = () => {
+
+    return {
+        ClientId: '46512c1c63b2452290415e484f8585cd',
+        ClientSecret: 'f45a83574baa41a8853ec3a5f52ee75c'
+    }
+}
+
+export { Credentials };
